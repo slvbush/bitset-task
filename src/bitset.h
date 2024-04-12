924c406850e36ade07e000c644bec4e1f4c79aad
Artem Koton <kotonartem@protonmail.com>
2024-04-13 01:11:51 +0300

    Add interface and README.md

---
+++ src/bitset.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <cstddef>
+#include <string_view>
+
+class bitset {
+public:
+  using value_type = bool;
+  using reference = void;
+  using const_reference = void;
+  using iterator = void;
+  using const_iterator = void;
+  using view = void;
+  using const_view = void;
+  using word_type = void;
+
+  static constexpr std::size_t npos = -1;
+
+  bitset();
+  bitset(std::size_t size, bool value);
+  bitset(const bitset& other);
+  explicit bitset(std::string_view str);
+  explicit bitset(const const_view& other);
+  bitset(const_iterator first, const_iterator last);
+
+  bitset& operator=(const bitset& other) &;
+  bitset& operator=(std::string_view str) &;
+  bitset& operator=(const const_view& other) &;
+
+  ~bitset();
+
+  void swap(bitset& other);
+
+  std::size_t size() const;
+  bool empty() const;
+
+  reference operator[](std::size_t index);
+  const_reference operator[](std::size_t index) const;
+
+  iterator begin();
+  const_iterator begin() const;
+
+  iterator end();
+  const_iterator end() const;
+
+  bitset& operator&=(const const_view& other) &;
+  bitset& operator|=(const const_view& other) &;
+  bitset& operator^=(const const_view& other) &;
+  bitset& operator<<=(std::size_t count) &;
+  bitset& operator>>=(std::size_t count) &;
+  void flip() &;
+
+  bitset& set() &;
+  bitset& reset() &;
+
+  bool all() const;
+  bool any() const;
+  std::size_t count() const;
+
+  operator const_view() const;
+  operator view();
+
+  view subview(std::size_t from = 0, std::size_t count = npos);
+  const_view subview(std::size_t from = 0, std::size_t count = npos) const;
+};
+
+bool operator==(const bitset& left, const bitset& right);
+bool operator!=(const bitset& left, const bitset& right);
+
+void swap(bitset& lhs, bitset& rhs);
